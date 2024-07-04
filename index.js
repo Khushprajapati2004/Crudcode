c2877d674a6d83568d9e9cc2c2c94668f3817714
khushprajapati1024 <khushprajapati777@gmail.com>
2024-07-04 08:34:52 +0530

    change

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 4000;
 
-
 app.get("/", (req, res) => {
   res.send("server on");
 });
 
-
-
-
 // Import the router files.
-const personRoutes = require('./routes/personRoutes');
-const menuRoutes = require('./routes/menuRoutes');
+const personRoutes = require("./routes/personRoutes");
+const menuRoutes = require("./routes/menuRoutes");
 
 // Use person routes
-app.use('/person', personRoutes);
-app.use('/menuitem', menuRoutes);
+app.use("/person", personRoutes);
+app.use("/menuitem", menuRoutes);
 
-
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-  });
-
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
